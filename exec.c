3e5570fe5623d424a7b07f8ff06758a496a89d2d
Abraham Algorithm <101641878+AbrahamAlgorithm@users.noreply.github.com>
2023-10-17 17:20:44 +0200

    Update exec.c

--- exec.c
+++ exec.c
@@ -1,54 +1,99 @@
 #include "shells.h"
 
-void execute_comand(const char *command)
+int execute_command(char **args)
 {
-    if (command == NULL || command[0] == '\0')
-    {
-        return;
-    }
-
-    char *args[64]; // Adjust the array size as needed
-    int arg_count = 0;
-
-    // Tokenize the command into arguments
-    char *token = strtok((char *)command, " ");
-    while (token != NULL && arg_count < 63)
-    {
-        args[arg_count++] = token;
-        token = strtok(NULL, " ");
-    }
-    args[arg_count] = NULL; // Null-terminate the array
-
-    if (strcmp(args[0], "cd") == 0)
-    {
-        if (args[1] != NULL)
+	/* Check if the command is not specified */
+	if (args[0] == NULL)
 	{
-            if (chdir(args[1]) != 0)
-	    {
-                perror("cd");
-            }
-        }
+		return (0); /* Successful execution */
+	}
+	/* Check if it's a 'cd' command */
+	if (strcmp(args[0], "cd") == 0)
+	{
+		if (args[1] == NULL)
+		{
+			/* Change to the home directory */
+			if (chdir(getenv("HOME")) != 0)
+			{
+				perror("cd");
+			}
+		}
+		else
+		{
+			/* Change to the specified directory */
+			if (chdir(args[1]) != 0)
+			{
+				perror("cd");
+			}
+		}
+		return (1); /* Command executed, but not an error */
+	}
+	/* Check if the command is found in the current directory */
+	if (access(args[0], X_OK) == 0)
+	{
+		pid_t pid = fork();
+		if (pid == 0)
+		{
+			/* Inside the child process */
+			if (execve(args[0], args, environ) == -1)
+			{
+				perror("execve");
+				exit(EXIT_FAILURE);
+			}
+		}
+		else if (pid < 0)
+		{
+			perror("fork");
+			return (-1);
+		}
+		else
+		{
+			/* Inside the parent process */
+			int status;
+			waitpid(pid, &status, 0);
+			if (WIFEXITED(status))
+			{
+				return WEXITSTATUS(status);
+			}
+		}
+		return (-1);
     }
-    else
-    {
-        pid_t child = fork();
-
-        if (child == -1)
+	
+	/* Check if the command is found in the /bin directory */
+	char command_path[100];
+	snprintf(command_path, sizeof(command_path), "/bin/%s", args[0]);
+	
+	if (access(command_path, X_OK) == 0)
 	{
-            perror("fork");
-        }
-	else if (child == 0)
-	{
-            // Child process
-            execvp(args[0], args);
-            perror("execvp");
-            exit(EXIT_FAILURE);
-        }
-	else
-	{
-            // Parent process
-            int status;
-            waitpid(child, &status, 0);
-        }
+		pid_t pid = fork();
+		if (pid == 0) 
+		{
+			/* Inside the child process */
+			if (execve(command_path, args, environ) == -1)
+			{
+				perror("execve");
+				exit(EXIT_FAILURE);
+			}
+		}
+		else if (pid < 0)
+		{
+			perror("fork");
+			return (-1);
+		}
+		else
+		{
+			/* Inside the parent process */
+			int status;
+			waitpid(pid, &status, 0);
+			if (WIFEXITED(status))
+			{
+				return WEXITSTATUS(status);
+			}
+		}
+		return (-1);
     }
+    
+    /* Command not found */
+	printf("Command not found\n");
+	return (-1);
 }
