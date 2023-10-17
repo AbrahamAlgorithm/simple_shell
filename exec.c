5770b2bdcd0be5011e5dc17e7f989a7cf52f8980
Abraham Algorithm <101641878+AbrahamAlgorithm@users.noreply.github.com>
2023-10-17 16:30:27 +0200

    Update exec.c

--- exec.c
+++ exec.c
@@ -2,28 +2,53 @@
 
 void execute_comand(const char *command)
 {
-	pid_t child_pid = fork ();
+    if (command == NULL || command[0] == '\0')
+    {
+        return;
+    }
 
-	if (child_pid == -1)
+    char *args[64]; // Adjust the array size as needed
+    int arg_count = 0;
+
+    // Tokenize the command into arguments
+    char *token = strtok((char *)command, " ");
+    while (token != NULL && arg_count < 63)
+    {
+        args[arg_count++] = token;
+        token = strtok(NULL, " ");
+    }
+    args[arg_count] = NULL; // Null-terminate the array
+
+    if (strcmp(args[0], "cd") == 0)
+    {
+        if (args[1] != NULL)
 	{
-		printf("Error forking process.\n");
-		exit(EXIT_FAILURE);
-	}
-	else if (child_pid == 0)
+            if (chdir(args[1]) != 0)
+	    {
+                perror("cd");
+            }
+        }
+    }
+    else
+    {
+        pid_t child = fork();
+
+        if (child == -1)
 	{
-		/*Child process*/
-		char *args[] = {"/bin/sh", "-c", command, NULL};
-		execvp(args[0], args);
-		perror("execvp");
-		exit(EXIT_FAILURE);
-	}
-	else if (pid < 0)
+            perror("fork");
+        }
+	else if (child == 0)
 	{
-		perror("fork");
-	}
+            // Child process
+            execvp(args[0], args);
+            perror("execvp");
+            exit(EXIT_FAILURE);
+        }
 	else
 	{
-		/*Parent process*/
-		int status;
-		waitpid(child_pid, &status, 0);
-	}
+            // Parent process
+            int status;
+            waitpid(child, &status, 0);
+        }
+    }
+}
