c8e424a229ffaf69872a3d6ec394c75f3b65f6a3
Abraham Algorithm <101641878+AbrahamAlgorithm@users.noreply.github.com>
2023-10-19 16:16:50 +0200

    Update strings.c

--- strings.c
+++ strings.c
@@ -1,110 +1,81 @@
 #include "shells.h"
 
 /**
- * _strcpy - Copies one string to another
- * @path: The destination string
- * @source: The source string
+ * _strlen - Returns the length of a string.
+ * @s: A pointer to the characters string.
  *
- * Return: Pointer to the path string
+ * Return: The length of the character string.
  */
-char *_strcpy(char *path, const char *source)
+int _strlen(const char *s)
 {
-	char *path_ptr = path;
+	int length = 0;
 
-	for (; *source != '\0'; source++, path_ptr++)
-	{
-		*path_ptr = *source;
-	}
-
-	*path_ptr = '\0';
-
-	return (path);
-}
-
-/**
- * _strcmp - Compare two strings
- * @s1: The first string
- * @s2: The second string
- *
- * Return: 0 if the strings are equal
- * +1 if s1 is greater
- * -1 is s2 is greater
- */
-int _strcmp(const char *s1, const char *s2)
-{
-	while (*s1 && (*s1 == *s2))
-	{
-		s1++;
-		s2++;
-	}
-	return (*(const unsigned char *)s1 - *(const unsigned char *)s2);
-}
-
-/**
- * _strconcat - Concatenates two strings
- * with a '/' char between them
- * @s1: The first string
- * @s2: The second string
- *
- * Return: A newly allocated string containing s1, '/', and s2
- */
-char *_strconcat(const char *s1, const char *s2)
-{
-	char *result;
-	size_t len1;
-	size_t len2;
-	size_t i;
-	len1 = 0;
-	len2 = 0;
-
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
-
-	while (s1[len1] != '\0')
-		len1++;
-
-	while (s2[len2] != '\0')
-		len2++;
-
-	result = (char *)malloc(len1 + len2 + 2);
-
-	if (result == NULL)
-	{
-		perror("Memory allocation error");
-		return (NULL);
-	}
-
-	for (i = 0; i < len1; i++)
-	{
-		result[i] = s1[i];
-	}
-
-	result[len1] = '/';
-
-	for (i = 0; i < len2; i++)
-	{
-		result[len1 + 1 + i] = s2[i];
-	}
-
-	result[len1 + len2 + 1] = '\0';
-
-	return (result);
-}
-
-/**
- * _strlen - Calculate the length of a string
- * @str: The input string
- *
- * Return: The length of the string.
- */
-size_t _strlen(const char *str)
-{
-	size_t len = 0;
-
-	if (!str)
-		return (len);
-	for (len = 0; str[len]; len++)
+	if (!s)
+		return (length);
+	for (length = 0; s[length]; length++)
 		;
-	return (len);
+	return (length);
 }
 
+/**
+ * _strcpy - Copies the string pointed to by src, including the
+ *           terminating null byte, to the buffer pointed by des.
+ * @dest: Pointer to the destination of copied string.
+ * @src: Pointer to the src of the source string.
+ *
+ * Return: Pointer to dest.
+ */
+char *_strcpy(char *dest, const char *src)
+{
+	size_t i;
+
+	for (i = 0; src[i] != '\0'; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
+}
+
+/**
+ * _strcat - Concantenates two strings.
+ * @dest: Pointer to destination string.
+ * @src: Pointer to source string.
+ *
+ * Return: Pointer to destination string.
+ */
+char *_strcat(char *dest, const char *src)
+{
+	char *destTemp;
+	const char *srcTemp;
+
+	destTemp = dest;
+	srcTemp =  src;
+
+	while (*destTemp != '\0')
+		destTemp++;
+
+	while (*srcTemp != '\0')
+		*destTemp++ = *srcTemp++;
+	*destTemp = '\0';
+	return (dest);
+}
+
+/**
+ * _strncat - Concantenates two strings where n number
+ *            of bytes are copied from source.
+ * @dest: Pointer to destination string.
+ * @src: Pointer to source string.
+ * @n: n bytes to copy from src.
+ *
+ * Return: Pointer to destination string.
+ */
+char *_strncat(char *dest, const char *src, size_t n)
+{
+	size_t dest_len = _strlen(dest);
+	size_t i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[dest_len + i] = src[i];
+	dest[dest_len + i] = '\0';
+
+	return (dest);
+}
