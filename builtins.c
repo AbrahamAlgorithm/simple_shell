04a8f847ce0ec321155030ce8f02a369779d8aec
Abraham Algorithm <101641878+AbrahamAlgorithm@users.noreply.github.com>
2023-10-21 23:28:54 +0200

    Update builtins.c

--- builtins.c
+++ builtins.c
@@ -1,6 +1,8 @@
-#include "shells.h"
-int hist;
-char *name;
+#include "shell.h"
+int (*get_builtin(char *command))(char **args, char **front);
+int shellby_exit(char **args, char **front);
+int shellby_cd(char **args, char __attribute__((__unused__)) **front);
+int shellby_help(char **args, char __attribute__((__unused__)) **front);
 
 /**
  * get_builtin - Matches a command with a corresponding
@@ -94,6 +96,7 @@ int shellby_cd(char **args, char __attribute__((__unused__)) **front)
 	oldpwd = getcwd(oldpwd, 0);
 	if (!oldpwd)
 		return (-1);
+
 	if (args[0])
 	{
 		if (*(args[0]) == '-' || _strcmp(args[0], "--") == 0)
@@ -103,12 +106,14 @@ int shellby_cd(char **args, char __attribute__((__unused__)) **front)
 			{
 				if (_getenv("OLDPWD") != NULL)
 					(chdir(*_getenv("OLDPWD") + 7));
-			} else
+			}
+			else
 			{
 				free(oldpwd);
 				return (create_error(args, 2));
 			}
-		} else
+		}
+		else
 		{
 			if (stat(args[0], &dir) == 0 && S_ISDIR(dir.st_mode)
 					&& ((dir.st_mode & S_IXUSR) != 0))
@@ -119,20 +124,26 @@ int shellby_cd(char **args, char __attribute__((__unused__)) **front)
 				return (create_error(args, 2));
 			}
 		}
-	} else
+	}
+	else
 	{
 		if (_getenv("HOME") != NULL)
 			chdir(*(_getenv("HOME")) + 5);
-	} pwd = getcwd(pwd, 0);
+	}
+
+	pwd = getcwd(pwd, 0);
 	if (!pwd)
 		return (-1);
+
 	dir_info = malloc(sizeof(char *) * 2);
 	if (!dir_info)
 		return (-1);
+
 	dir_info[0] = "OLDPWD";
 	dir_info[1] = oldpwd;
 	if (shellby_setenv(dir_info, dir_info) == -1)
 		return (-1);
+
 	dir_info[0] = "PWD";
 	dir_info[1] = pwd;
 	if (shellby_setenv(dir_info, dir_info) == -1)
@@ -141,7 +152,8 @@ int shellby_cd(char **args, char __attribute__((__unused__)) **front)
 	{
 		write(STDOUT_FILENO, pwd, _strlen(pwd));
 		write(STDOUT_FILENO, new_line, 1);
-	} free(oldpwd);
+	}
+	free(oldpwd);
 	free(pwd);
 	free(dir_info);
 	return (0);
